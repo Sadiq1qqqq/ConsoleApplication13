5b8492af1b38b7e8003d95efc33835c4d7468a9d
Sadiq Quliyev <sadiq000k@gmail.com>
2024-08-07 22:53:44 +0400

    Proje dosyası ekle.

---
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,153 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*for (int n = 1; n <= 10; n++)
+	{
+		for (int m = 1; m <= 10; m++)
+		{
+			cout << n * m << "	";
+		}
+
+		cout << endl << endl;
+	}*/
+
+
+	/*cout << "Eded daxil edin: ";
+	int num;
+	cin >> num;
+	for (int n = 0; n < num; n++)
+	{
+		for (int m = 0; m < num; m++)
+		{
+			if (n == 0 || n == num - 1)
+				cout << "*";
+			else if (m == 0 || m == num - 1)
+				cout << "*";
+			else if (n < num /3 - 1&& m < num / 3 - 1)
+				cout << "*";
+			else if (n < num /3 - 1&& m > num - num / 3)
+				cout << "*";
+			else if (n > num - num/3 && m < num / 3 - 1)
+				cout << "*";
+			else if (n > num - num/3 && m > num - num / 3)
+				cout << "*";
+			else
+				cout << " ";
+
+		}
+		cout << endl;
+	}cout << endl << endl;*/
+
+
+	//cout << "Eded daxil edin: ";
+	// 
+	// 
+	//int num;
+	//cin >> num;
+	//for (int n = 0; n < num; n++)
+	//{
+	//	for (int m = 0; m < num; m++)
+	//	{
+	//		if (n == num - m)
+	//			cout << "*";
+	//		else
+	//			cout << " ";
+
+	//	}
+	//	for (int m = 0; m < num; m++)
+	//	{
+	//		if (n == m)
+	//			cout << "*";
+	//		else
+	//			cout << " ";
+
+	//	}
+
+	//	cout << endl;
+	//}
+	//for (int n = 0; n < num * 2 + 1; n++)
+	//{
+	//	cout << "*";
+	//}
+	//cout << endl << endl;
+
+
+
+
+	/*cout << "Eded daxil edin: ";
+	int num;
+	cin >> num;
+	for (int n = 0; n < num; n++)
+	{
+		for (int m = 0; m < num; m++)
+		{
+			if (n >= num - m)
+				cout << "*";
+			else
+				cout << " ";
+
+		}
+		for (int m = 0; m < num; m++)
+		{
+			if (n >= m)
+				cout << "*";
+			else
+				cout << " ";
+
+		}
+
+		cout << endl;
+	}
+cout << endl << endl;*/
+
+
+	cout << "Eded daxil edin:";
+	int num;
+	cin >> num;
+	for (int n = 0; n < num; n++)
+	{
+		for (int m = 0; m < num; m++)
+		{
+			if (n == num - m)
+				cout << "*";
+			else
+				cout << " ";
+
+		}
+		for (int m = 0; m < num; m++)
+		{
+			if (n == m)
+				cout << "*";
+			else
+				cout << " ";
+
+		}
+		cout << endl;
+	}
+	for (int n = 0; n < num + 2; n++)
+	{
+		for (int m = 0; m < num; m++)
+		{
+			if (n == m)
+				cout << "*";
+			else
+				cout << " ";
+
+		}
+
+		for (int m = 0; m < num + 2; m++)
+		{
+			if (n == num - m)
+				cout << "*";
+			else
+				cout << " ";
+
+		}
+
+cout << endl;
+	}
+cout << endl << endl;
+
